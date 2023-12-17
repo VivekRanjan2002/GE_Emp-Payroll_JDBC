525fb0403638c2af2610ed4cfbcfd2a074942778
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-12-18 04:15:03 +0530

    Added:UC5 JUNIT test cases

--- test/EmpPayrollTest.java
+++ test/EmpPayrollTest.java
@@ -4,6 +4,7 @@ import com.bridgeLabz.Main.service.entity.EmployeePayrollData;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static com.bridgeLabz.Main.service.EmployeePayrollService.IOService.DB_IO;
@@ -26,5 +27,17 @@ public class EmpPayrollTest {
         employeePayrollService.updateEmployeeSalary("Terrisa", 3000000);
         boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Terrisa");
         Assert.assertTrue(result);
+    }
+    // UC5
+    @Test
+    public void givenDateRange_When_Retrieved__ShouldMatch_EmployeeCount(){
+        EmployeePayrollService employeePayrollService= new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData= employeePayrollService.readEmployeePayrollData(DB_IO);
+        LocalDate startDate= LocalDate.of(2023,10,10);
+        LocalDate endDate= LocalDate.now();
+        List<EmployeePayrollData> employeePayroll= employeePayrollService.readEmployeePayrollDataforGivenDateRange(DB_IO,startDate,endDate);
+        Assert.assertEquals(3,employeePayroll.size());
+
+
     }
 }
